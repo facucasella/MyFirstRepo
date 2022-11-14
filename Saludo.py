71b9a4e2111075d440d658ddc8a166c80fc9dfd2
facu <facu.casella95@gmail.com>
2022-11-14 17:16:36 -0300

    MiPrimerCommit

---
+++ Saludo.py
@@ -0,0 +1,3 @@
+saludo=str(input('Hola, bienvenido a mi primer repositorio! ¿Cual es tu nombre?: '))
+print('Bienvenido',saludo.capitalize(),'gracias por probar mi primer repo :)')
+print('Esto es una prueba para el git bush :D')
